4f949b5fa3037444db2c14cc24857a4bac029431
lukas88 <lukasz.szymanski@labnoratory.com>
2017-02-17 23:36:48 +0100

    some changes

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -8,3 +8,14 @@ $(document).ready(function(){
 
     });
 });
+
+$(document).ready(function(){
+
+    $(".girls").owlCarousel({
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+        items : 1,
+        itemsDesktop : [1199,3],
+        itemsDesktopSmall : [979,3]
+
+    });
+});
