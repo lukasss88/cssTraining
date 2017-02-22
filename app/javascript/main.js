68b7f9e27ce68ad3495360bb597a33fbf8f2753b
lukas88 <lukasz.szymanski@labnoratory.com>
2017-02-22 22:25:08 +0100

    create preloader

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
 
+    /*PRELOADER JS*/
+    $(window).on('load', function() {
+        $('.status').fadeOut();
+        $('.preloader').delay(350).fadeOut('slow');
+    });
+    /*END PRELOADER JS*/
+
     $(".partner").owlCarousel({
         autoPlay: 3000, //Set AutoPlay to 3 seconds
         items : 4,
@@ -7,15 +14,31 @@ $(document).ready(function(){
         itemsDesktopSmall : [979,3]
 
     });
-});
 
-$(document).ready(function(){
-
-    $(".girls").owlCarousel({
-        autoPlay: 3000, //Set AutoPlay to 3 seconds
-        items : 1,
-        itemsDesktop : [1199,3],
-        itemsDesktopSmall : [979,3]
-
+    $('a.page-scroll').on('click', function(e){
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top - 50
+        }, 1500);
+        e.preventDefault();
     });
+
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() > 100) {
+            $('.menu-top').addClass('menu-shrink');
+        } else {
+            $('.menu-top').removeClass('menu-shrink');
+        }
+    });
+
+    $("#about-carousel").hover(function(){
+        $(".carousel .slide-control.left").toggleClass("hover-left-arrow");
+        $(".carousel .slide-control.right").toggleClass("hover-right-arrow")
+    });
+
+    new WOW().init();
+
 });
+
+
+
